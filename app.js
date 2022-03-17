98a2b580eae9e2e1a69e5c0cd43d0a4f130aec10
minhquan01 <quanne112001@gmail.com>
2022-03-17 23:23:47 +0700

    upload code

---
+++ app.js
@@ -0,0 +1,12 @@
+
+var nav = document.getElementById("nav")
+window.onscroll = function() {
+    if (document.documentElement.scrollTop > 50 || document.body.scrollTop > 0) {
+        console.log(document.documentElement.scrollTop);
+        nav.style.backgroundColor = "#fff";
+        nav.style.boxShadow ="-1px 1px 1px #000";
+    } else {
+        nav.style.backgroundColor = "hsla(0,0%,100%,.75)";
+        nav.style.boxShadow ="-1px 1px 1px transparent";
+    }
+}
