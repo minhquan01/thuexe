7004d276765d239742db86e9045626dd0fbbd080
minhquan01 <quanne112001@gmail.com>
2022-03-29 11:42:18 +0700

    Enter

---
+++ search.js
@@ -0,0 +1,57 @@
+const nameItems = document.querySelectorAll('.name-item');
+const searchInput = document.querySelector('.search-input');
+const itemMotos = document.querySelectorAll('.item-moto');
+const buttonSuggests = document.querySelectorAll('.search-suggest-button--btn');
+const priceItems = document.querySelectorAll('.price-item');
+
+// Tìm Kiếm
+searchInput.onkeyup = () => {
+    let valueItem = searchInput.value.toLowerCase();
+    itemMotos.forEach((ele) => {
+        let nameMoto = ele.querySelector('.item-info').firstElementChild.textContent;
+        if(nameMoto.toLowerCase().indexOf(valueItem) !== -1) {
+            ele.style.display = 'block';
+        } else {
+            ele.style.display = 'none';
+        }
+    })
+
+    checkEmpty(itemMotos);
+}
+
+// Check sản phẩm có không?
+
+function checkEmpty(ele) {
+    let count = 0;
+    for(let i = 0; i < ele.length; i++) {
+        if(ele[i].style.display === 'block'){
+            count ++;
+        }
+    }
+
+    if(count === 0){
+        document.querySelector('#showtext').textContent = 'Không Tìm Thấy';
+    }else{
+        document.querySelector('#showtext').textContent = '';
+    }
+}
+
+
+// Lọc
+
+buttonSuggests.forEach((suggest) => {
+    suggest.onclick = () => {
+        document.querySelector('.search-suggest-button--btn.active').classList.remove('active')
+        suggest.classList.add('active')
+
+        let nameSuggest =suggest.dataset.search;
+    
+        itemMotos.forEach((ele) => {
+            if(ele.dataset.item === nameSuggest || nameSuggest === 'all'){
+                ele.style.display = 'block';
+            }else {
+                ele.style.display = 'none';
+            }
+        })
+    }
+})
